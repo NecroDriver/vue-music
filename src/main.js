6fc39ecc98ca4243a56ddeac6107d1841e2f22e0
mafh <1193482868@qq.com>
2018-06-04 18:21:26 +0800

    封面3-2

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
 
+import 'common/less/index.less'
+
 Vue.config.productionTip = false
 
 fastclick.attach(document.body)
